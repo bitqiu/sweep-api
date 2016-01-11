78c1788d2fa991f71b38e18363ab8dda6ff64934
BitQiu <i@bitqiu.cc>
2016-01-12 00:57:29 +0800

    update

--- config/app.php
+++ config/app.php
@@ -141,6 +141,7 @@ return [
          * Package Service Providers...
          */
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -195,6 +196,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Debugbar'  => Barryvdh\Debugbar\Facade::class,
 
     ],
 
